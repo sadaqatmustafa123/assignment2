fe004a4f794dd96d426e2cd69e867b74e165fe7b
sadaqatmustafa123 <sadaqatmustafa12345@gmail.com>
2023-06-24 17:03:04 +0500

    add question 1 and 2

---
+++ question1.dart
@@ -0,0 +1,9 @@
+void main() {
+  // Create a list of names
+  List<String> names = ['Sadaqat', 'Ali', 'Mustafa', 'Kumail', 'Shahzaib'];
+
+  // Print all the names using a loop
+  for (String name in names) {
+    print(name);
+  }
+}
