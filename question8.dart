9f8d14e7e332050cde410213fa5bfdedc978b1ec
sadaqatmustafa123 <sadaqatmustafa12345@gmail.com>
2023-06-24 17:45:28 +0500

    added another question

---
+++ question8.dart
@@ -0,0 +1,16 @@
+import 'dart:ffi';
+
+void main() {
+  List<Map<String, bool>> usersEligibility = [
+    {'name': 'John', 'eligible': true},
+    {'name': 'Alice', 'eligible': false},
+    {'name': 'Mike', 'eligible': true},
+    {'name': 'Sarah', 'eligible': true},
+    {'name': 'Tom', 'eligible': false},
+  ];
+
+  usersEligibility.removeWhere((user) => user['eligible'] == false);
+
+  print('Updated List:');
+  print(usersEligibility);
+}
